98af6f98068771bb90663076fbceb76400d7c25c
Masoud Heydari <mhbargezard@gmail.com>
2020-07-28 23:26:25 +0430

    Add project files.

---
+++ Practice/Models/Contexts/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.Models.Contexts
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=MASOUD-PC\MBZSERVER; Initial Catalog=StoreEF; Integrated Security=True;");
+        }
+    }
+}
